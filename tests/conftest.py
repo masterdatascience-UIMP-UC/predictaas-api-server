e6a912d6342a98109ddbd0b4710dcc93bb80b832
Pablo Calatayud <pablocalatayudpelayo@gmail.com>
2022-03-14 17:39:00 +0100

    commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,5 +17,5 @@ def client(loop, aiohttp_client):
     app = connexion.AioHttpApp(__name__, specification_dir=specification_dir,
                                options=options)
     app.add_api('openapi.yaml', pythonic_params=True,
-                pass_context_arg_name='request')
+                word_context_arg_name='request')
     return loop.run_until_complete(aiohttp_client(app.app))
